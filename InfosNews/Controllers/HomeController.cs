c6deca63a14fbd7e2e56f1dab2560457865661e0
GlobusCrea <globuscrea@gmail.com>
2015-02-04 13:03:46 +0100

    DAL + View Index (News)

---
+++ InfosNews/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using InfosNews.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InfosNews.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            List<News> mesNews = News.getAllNews();
+            return View(mesNews);
+        }
+	}
+}
