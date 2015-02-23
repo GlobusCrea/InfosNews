799d94283603f05b642d1ef2f4475fac328e47e5
GlobusCrea <globuscrea@gmail.com>
2015-02-23 11:03:32 +0100

    Push All

---
+++ InfosNews.DAL/Auteurs.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfosNews.DAL
+{
+    public class Auteurs
+    {
+        #region Fields
+
+        private int _idAuteur;
+        private string _AuteurNom;
+        private string _AuteurPrenom;
+        private int _AuteurPower;
+        private string _AuteurLogin;
+        private string _AuteurPassword;
+        private List<News> _News;
+
+        #endregion
+
+        #region Properties
+
+        public int idAuteur
+        {
+            get { return _idAuteur; }
+            set { _idAuteur = value; }
+        }
+
+        public string auteurNom
+        {
+            get { return _AuteurNom; }
+            set { _AuteurNom = value; }
+        }
+
+        public string auteurPrenom
+        {
+            get { return _AuteurPrenom; }
+            set { _AuteurPrenom = value; }
+        }
+
+        public int auteurPower
+        {
+            get { return _AuteurPower; }
+            set { _AuteurPower = value; }
+        }
+
+        public string auteurLogin
+        {
+            get { return _AuteurLogin; }
+            set { _AuteurLogin = value; }
+        }
+
+        public string auteurPassword
+        {
+            get { return _AuteurPassword; }
+            set { _AuteurPassword = value; }
+        }
+
+        public List<News> news
+        {
+            get { return _News = _News ?? getNewsByAuteur(this.idAuteur); }
+        }
+
+        #endregion
+
+        #region Static
+
+        public static Auteurs getChampsAuteurs(Dictionary<string,object> item)
+        {
+            Auteurs aut = new Auteurs();
+            aut.idAuteur = (int)item["idAuteur"];
+            aut.auteurNom = item["AuteurNom"].ToString();
+            aut.auteurPrenom = item["AuteurPrenom"].ToString();
+            aut.auteurPower = (int)item["AuteurPower"];
+            aut.auteurLogin = item["AuteurLogin"].ToString();
+            aut.auteurPassword = item["AuteurPassword"].ToString();
+            return aut;
+        }
+
+        public static Auteurs getOneAuteur(int idAuteur)
+        {
+            List<Dictionary<string, object>> unAuteur = GestionConnexion.Instance.getData("SELECT * FROM tblAuteurs WHERE idAuteur =" + idAuteur);
+            Auteurs auteur = getChampsAuteurs(unAuteur[0]);
+            return auteur;
+        }
+
+        public static List<Auteurs> getAllAuteurs()
+        {
+            List<Dictionary<string, object>> desAuteurs = GestionConnexion.Instance.getData("SELECT * FROM tblAuteurs");
+            List<Auteurs> lstAuteurs = new List<Auteurs>();
+            foreach(Dictionary<string,object> item in desAuteurs)
+            {
+                Auteurs auteur = getChampsAuteurs(item);
+                lstAuteurs.Add(auteur);
+            }
+            return lstAuteurs;
+        }
+
+        public static News getChampsNews(Dictionary<string,object> item)
+        {
+            News news = new News();
+            news.idNews = (int)item["idNews"];
+            news.idAuteur = (int)item["idAuteur"];
+            news.NewsPicture = item["NewsPicture"].ToString();
+            news.NewsDate = (DateTime)item["NewsDate"];
+            news.NewsTitre = item["NewsTitre"].ToString();
+            news.NewsTxt = item["NewsTxt"].ToString();
+            news.NewsResume = item["NewsResume"].ToString();
+            return news;
+        }
+
+        #endregion
+
+        #region Function
+
+        public static List<News> getNewsByAuteur(int id)
+        {
+            List<Dictionary<string,object>> nba = GestionConnexion.Instance.getData("SELECT * FROM tblNews WHERE idAuteur = " + id);
+            List<News> lstnba = new List<News>();
+            foreach(Dictionary<string,object> item in nba)
+            {
+                News news = getChampsNews(item);
+                lstnba.Add(news);
+            }
+            return lstnba;
+        }
+
+      
+        /// <summary>
+        /// Ajouter ou modifier un auteur
+        /// </summary>
+        /// <returns></returns>
+        public bool saveAuteur()
+        {
+            string strRequest = "";
+
+            if(this.idAuteur == 0)
+            {
+                strRequest = "INSERT INTO tblAuteurs (AuteurNom,AuteurPrenom,AuteurPower,AuteurLogin,AuteurPassword) VALUES (@AuteurNom, @AuteurPrenom, @AuteurPower, @AuteurLogin, @AuteurPassword)";
+            }
+            else
+            {
+                strRequest = "UPDATE tblAuteurs SET AuteurNom = @AuteurNom,AuteurPrenom = @AuteurPrenom,AuteurPower = @AuteurPower,AuteurLogin = @AuteurLogin,AuteurPassword = @AuteurPassword";
+            }
+
+            Dictionary<string, object> ValueToSave = new Dictionary<string, object>();
+            ValueToSave["AuteurNom"] = this.auteurNom;
+            ValueToSave["AuteurPrenom"] = this.auteurPrenom;
+            ValueToSave["AuteurPower"] = this.auteurPower;
+            ValueToSave["AuteurLogin"] = this.auteurLogin;
+            ValueToSave["AuteurPassword"] = this.auteurPassword;
+
+            if(GestionConnexion.Instance.saveData(strRequest, GenerateKey.DB, ValueToSave))
+            {
+                if (this.idAuteur == 0) GestionConnexion.Instance.getLastGenerateId();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Supprimer un auteur
+        /// </summary>
+        /// <returns></returns>
+        public bool deleteAuteur()
+        {
+            string strRequest = "DELETE FROM tblAuteurs WHERE idAuteur = @idAuteur";
+            Dictionary<string, object> ValueToDelete = new Dictionary<string, object>();
+            ValueToDelete["idAuteur"] = this.idAuteur;
+            return GestionConnexion.Instance.saveData(strRequest, GenerateKey.APP, ValueToDelete);
+        }
+
+        #endregion
+    }
+}
---
+++ InfosNews.DAL/News.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfosNews.DAL
+{
+    public class News
+    {
+        #region Fields
+
+        private int _idNews;
+        private int _idAuteur;
+        private string _newsPicture;
+        private DateTime _newsDate;
+        private string _newsTitre;
+        private string _newsTxt;
+        private string _newsResume;
+        private Auteurs _Auteur;
+
+        #endregion
+
+        #region Properties
+
+        public int idNews
+        {
+            get { return _idNews; }
+            set { _idNews = value; }
+        }
+
+        public int idAuteur
+        {
+            get { return _idAuteur; }
+            set { _idAuteur = value; }
+        }
+
+        public string NewsPicture
+        {
+            get { return _newsPicture; }
+            set { _newsPicture = value; }
+        }
+
+        public DateTime NewsDate
+        {
+            get { return _newsDate; }
+            set { _newsDate = value; }
+        }
+
+        public string NewsTitre
+        {
+            get { return _newsTitre; }
+            set { _newsTitre = value; }
+        }
+
+        public string NewsTxt
+        {
+            get { return _newsTxt; }
+            set { _newsTxt = value; }
+        }
+
+        public string NewsResume
+        {
+            get { return _newsResume; }
+            set { _newsResume = value; }
+        }
+
+        public Auteurs Auteur
+        {
+            get { return _Auteur = _Auteur ?? getAuteur(); }
+        }
+
+        #endregion
+
+        #region Static
+
+        public static News getChampsNews(Dictionary<string,object> item)
+        {
+            News news = new News();
+            news.idNews = (int)item["idNews"];
+            news.idAuteur = (int)item["idAuteur"];
+            news.NewsPicture = item["NewsPicture"].ToString();
+            news.NewsDate = (DateTime)item["NewsDate"];
+            news.NewsTitre = item["NewsTitre"].ToString();
+            news.NewsTxt = item["NewsTxt"].ToString();
+            news.NewsResume = item["NewsResume"].ToString();
+            return news;
+        }
+
+        public static News getOneNews(int idNews)
+        {
+            List<Dictionary<string, object>> uneNews = GestionConnexion.Instance.getData("SELECT * FROM tblNews WHERE idNews =" + idNews);
+            News news = getChampsNews(uneNews[0]);
+            return news;
+        }
+
+        public static List<News> getFiveNews()
+        {
+            List<Dictionary<string, object>> fiveNews = GestionConnexion.Instance.getData("SELECT TOP 5 * FROM dbo.tblNews ORDER BY NewsDate DESC");
+            List<News> lstFiveNews = new List<News>();
+            foreach (Dictionary<string, object> item in fiveNews)
+            {
+                News news = getChampsNews(item);
+                lstFiveNews.Add(news);
+            }
+            return lstFiveNews;
+        }
+
+        public static List<News> getAllNews()
+        {
+            List<Dictionary<string, object>> desNews = GestionConnexion.Instance.getData("SELECT * FROM tblNews");
+            List<News> lstNews = new List<News>();
+            foreach(Dictionary<string,object> item in desNews)
+            {
+                News news = getChampsNews(item);
+                lstNews.Add(news);
+            }
+            return lstNews;
+        }
+
+        #endregion
+
+        #region Function
+
+        public Auteurs getAuteur()
+        {
+            string strRequest = @"SELECT AuteurPrenom,AuteurNom FROM tblAuteurs a INNER JOIN tblNews n ON a.idAuteur = n.idAuteur WHERE n.idAuteur = " + this.idAuteur;
+            List<Dictionary<string, object>> auteur = GestionConnexion.Instance.getData(strRequest);
+            Auteurs monAuteur = new Auteurs();
+            monAuteur.auteurPrenom = auteur[0]["AuteurPrenom"].ToString();
+            monAuteur.auteurNom = auteur[0]["AuteurNom"].ToString();
+            return monAuteur;
+        }
+
+
+        /// <summary>
+        /// Ajouter ou modifier une news
+        /// </summary>
+        /// <returns></returns>
+        public bool saveNews()
+        {
+            string strRequest = "";
+
+            if(this.idNews == 0)
+            {
+                strRequest = "INSERT INTO tblNews (idAuteur,NewsPicture,NewsDate,NewsTitre,NewsTxt,NewsResume) VALUES (@idAuteur,@NewsPicture,@NewsDate,@NewsTitre,@NewsTxt,@NewsResume)";
+            }
+            else
+            {
+                strRequest = "UPDATE tblAuteurs SET idAuteur = @idAuteur,NewsPicture = @NewsPicture,NewsDate = @NewsDate,NewsTitre = @NewsTitre,NewsTxt = @NewsTxt,NewsResume = @NewsResume ";
+            }
+
+            Dictionary<string, object> ValueToSave = new Dictionary<string, object>();
+            ValueToSave["idAuteur"] = this.idAuteur;
+            ValueToSave["NewsPicture"] = this.NewsPicture;
+            ValueToSave["NewsDate"] = this.NewsDate;
+            ValueToSave["NewsTitre"] = this.NewsTitre;
+            ValueToSave["NewsTxt"] = this.NewsTxt;
+            ValueToSave["NewsResume"] = this.NewsResume;
+
+            if(GestionConnexion.Instance.saveData(strRequest, GenerateKey.DB, ValueToSave))
+            {
+                if(this.idNews == 0) GestionConnexion.Instance.getLastGenerateId();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Supprimer une news
+        /// </summary>
+        /// <returns></returns>
+        public bool deleteNews()
+        {
+            string strRequest = "DELETE FOM tblNews WHERE idNews = @idNews";
+            Dictionary<string, object> ValueToDelete = new Dictionary<string, object>();
+            ValueToDelete["idNews"] = this.idNews;
+            return GestionConnexion.Instance.saveData(strRequest, GenerateKey.APP, ValueToDelete);
+        }
+
+        #endregion
+    }
+}
